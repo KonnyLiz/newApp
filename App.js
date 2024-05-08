b59b93d9c383e8b6c142be0cf01a4bb42b21243e
Konny Amaya <konnylizamaya@gmail.com>
2024-05-07 19:52:53 -0600

    :sparkles: Add children props

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+const Label = (props) => {
+  return (
+    <Text>
+      {props.children || 'Default Text'}
+    </Text>
+  )
+}
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>
+        <Label>
+          Konny y Joseph
+          <Label />
+        </Label>
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
